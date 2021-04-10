6751790e6f2a2f9900036acd1ad79da81364242c
wutchara <SSTMUHAM@gmail.com>
2021-04-10 14:24:05 +0700

    Variable

---
+++ variable.js
@@ -0,0 +1,18 @@
+console.log('HAM');
+// const
+const name = 'Wutchara';
+console.log('name:', name);
+// name = 'XXX';
+// console.log('name:', name);
+
+// let
+let name2 = 'XXX';
+console.log('name2:', name2);
+name2 = 'GG';
+console.log('name2:', name2);
+
+// var
+var name3 = 'ABC';
+console.log('name3:', name3);
+name3 = 'Zz';
+console.log('name3:', name3);
